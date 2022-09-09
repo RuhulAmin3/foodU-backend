5450bb2b41c28bf58cec725e4ba9b5d420a9d43d
Md. Al Amin <86846863+akalaminamin@users.noreply.github.com>
2022-09-09 14:26:42 +0600

    Update index.js
    Fixed cors policy error

--- index.js
+++ index.js
@@ -9,9 +9,7 @@ require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 // parsor
-app.use(cors({
-    origin: "*"
-}));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 
